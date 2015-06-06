beac3aeb8ec9379c6c33f9298c68b908e3a55319
dwester83 <dwester83@gmail.com>
2015-06-06 15:20:37 -0500

    Example layout

---
+++ Network/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public class Class1
+    {
+        // We'll add the Entitiy framework / Web services in here if we have that eventually
+    }
+}
