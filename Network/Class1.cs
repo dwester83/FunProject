cd26316ab66993799e2e5759fab985ae01d6d13d
dwester83 <dwester83@gmail.com>
2015-06-10 20:22:51 -0500

    More

--- Network/Class1.cs
+++ Network/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Network
+namespace Game_Network
 {
     public class Class1
     {
--- Network/Properties/AssemblyInfo.cs
+++ Network/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Network")]
+[assembly: AssemblyTitle("Game_Network")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Network")]
+[assembly: AssemblyProduct("Game_Network")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
