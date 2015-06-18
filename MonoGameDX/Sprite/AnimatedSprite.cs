53b0cfc52d864d2e0059e0e381bf0196d8e7bb28
Josh Hanke <josh.hanke@gmail.com>
2015-06-17 21:26:10 -0500

    tile changes( added animated sprite startings)

---
+++ MonoGameDX/Sprite/AnimatedSprite.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_DX.Sprite
+{
+    public class AnimatedSprite : AbstractSprite
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MonoGameDX/Tile.cs
+++ MonoGameDX/Tile.cs
@@ -14,15 +14,16 @@ namespace Game_DX
         public Vector2 Location { get; set; }
         public Vector2 Center 
         {
-            get { return new Vector2(Height/2, Width/2) ;}
+            get { return new Vector2(Location.X + (Width/2), Location.Y + (Height/2)) ; }
         }
         public float Height { get; set; }
         public float Width { get; set; }
         public object CollisionObject { get; set; }
+        public object  Tag { get; set; }
         public bool isSolid { get; set; }
         public bool isMoveable { get; set; }
 
-        public Tile()
+        public Tile(Vector2 location)
         {
 
         }
@@ -38,7 +39,7 @@ namespace Game_DX
 
         public void draw(SpriteBatch spritebatch)
         {
-
+            sprite.draw(spritebatch);
         }
 
     }
